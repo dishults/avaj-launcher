1f3c60024dd045f71cc03cd67766e665c76caf74
Dmytro Shults <shults@dmytros-mini.lan>
2020-06-26 15:10:39 +0200

    Update init.java description

--- avaj/Init.java
+++ avaj/Init.java
@@ -5,15 +5,17 @@ This file is necessary so that the following command would work:
 
 Because normally there should be a dot, like so:
     javac -sourcepath . @sources.txt
+Or:
+    javac -sourcepath avaj @sources.txt
 
 Because "@file" just expands the file for java compiler 
 and lists all the entries. 
 So the first entry from the file will be taken by sourcepath
-which will use it as a reference for the path of where to look for classes. 
+which will use it as a reference for the path of where to look for sources. 
 As a result the first file from sources.txt won't be compiled
-and you will have lots of errors poppin up.
+and you will have lots of errors popping up.
 
-So if we want to successfully compile the project with the given command, 
+So in order to successfully compile the project with the given command, 
 this file is absolutely necessary.
 
 After running:
